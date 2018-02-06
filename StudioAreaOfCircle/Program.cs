a121a4fd354f7003067a060f465c6f3436e7b756
Daniel <danielbrogers594@gmail.com>
2018-02-06 16:45:02 -0500

    changed type to double

--- StudioAreaOfCircle/Program.cs
+++ StudioAreaOfCircle/Program.cs
@@ -6,9 +6,9 @@ namespace StudioAreaOfCircle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the radius of the circle, then press ENTER ");
-            string radius = Console.ReadLine();
-            double r = Convert.ToInt32(radius);
+            Console.WriteLine("Please enter the radius of the circle, then press ENTER " ) ;
+
+            double r = Double.Parse(Console.ReadLine());                      
 
             double area = 3.14 * r * r;
 
