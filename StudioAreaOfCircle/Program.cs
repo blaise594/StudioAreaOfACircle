698ba45936cebf07ca11cf12dc7476068b036fd6
Daniel <danielbrogers594@gmail.com>
2018-02-06 16:26:23 -0500

    first commit

---
+++ StudioAreaOfCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StudioAreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radius of the circle, then press ENTER ");
+            string radius = Console.ReadLine();
+            double r = Convert.ToInt32(radius);
+
+            double area = 3.14 * r * r;
+
+            Console.WriteLine("The area of the circle is: " + area);
+            Console.ReadLine();
+            
+        }
+    }
+}
