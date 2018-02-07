95860e3d211001eae37c219bd58081b53145f890
Daniel <danielbrogers594@gmail.com>
2018-02-06 19:01:01 -0500

    added if else statements

--- StudioAreaOfCircle/Program.cs
+++ StudioAreaOfCircle/Program.cs
@@ -6,15 +6,34 @@ namespace StudioAreaOfCircle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the radius of the circle, then press ENTER " ) ;
-
-            double r = Double.Parse(Console.ReadLine());                      
-
-            double area = 3.14 * r * r;
-
-            Console.WriteLine("The area of the circle is: " + area);
-            Console.ReadLine();
             
+          
+            Console.WriteLine("Please enter the radius of the circle, then press ENTER " ) ;
+            
+                double r = Double.Parse(Console.ReadLine());
+                        
+                if (r > 0)
+                {
+                    double area = 3.14 * r * r;
+
+                    Console.WriteLine("The area of the circle is: " + area);
+                    Console.ReadLine();
+                }
+                else if (r <= 0)
+                {
+
+                    Console.WriteLine("Please enter a positive number");
+                    Console.ReadLine();
+                
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    Console.ReadLine();
+
+                }
+            
+
         }
     }
 }
