5e828ec325afd8c30cdadc7c2575aa9c6d9dfe32
Daniel <danielbrogers594@gmail.com>
2018-02-06 23:51:07 -0500

    added try catch

--- StudioAreaOfCircle/Program.cs
+++ StudioAreaOfCircle/Program.cs
@@ -6,12 +6,12 @@ namespace StudioAreaOfCircle
     {
         static void Main(string[] args)
         {
-            
-          
-            Console.WriteLine("Please enter the radius of the circle, then press ENTER " ) ;
-            
+
+
+            Console.WriteLine("Please enter the radius: ");
+            try
+            {
                 double r = Double.Parse(Console.ReadLine());
-                        
                 if (r > 0)
                 {
                     double area = 3.14 * r * r;
@@ -21,18 +21,16 @@ namespace StudioAreaOfCircle
                 }
                 else if (r <= 0)
                 {
-
-                    Console.WriteLine("Please enter a positive number");
+                    Console.WriteLine("The radius must be a positive number");
                     Console.ReadLine();
-                
                 }
-                else
-                {
-                    Console.WriteLine("Please enter a valid number");
-                    Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid character");
+                Console.ReadLine();
+            }
 
-                }
-            
 
         }
     }
